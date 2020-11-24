495b48f193d3ba9c4c4a81ccdbd6ee2fc7aab8e1
mikilaus <41994169+mikilaus@users.noreply.github.com>
2020-11-24 12:38:35 +0100

    build

---
+++ build/precache-manifest.8e90e254a1649cafaf2cde216f1291f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64b618a720e8995a05a4fe885c515672",
+    "url": "/index.html"
+  },
+  {
+    "revision": "662419c1d3da36b2ae41",
+    "url": "/static/css/main.3d9f8e8d.chunk.css"
+  },
+  {
+    "revision": "e1a3c50a15b44825207f",
+    "url": "/static/js/2.bdbc782b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.bdbc782b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "662419c1d3da36b2ae41",
+    "url": "/static/js/main.6219c1dd.chunk.js"
+  },
+  {
+    "revision": "7402b3a51c6a0325ccf5",
+    "url": "/static/js/runtime-main.297f6f71.js"
+  }
+]);
